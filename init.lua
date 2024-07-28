1435187b0d8981ae6a5907f37bd665d2e52b930d
MuraKon2906 <mcray2906writes@gmail.com>
2024-07-28 17:13:58 +0530

    neo tree added

--- init.lua
+++ init.lua
@@ -27,18 +27,33 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
 
 
  	  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-		 {
+		 
+
+    {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
 
       dependencies = { 'nvim-lua/plenary.nvim' }
     },
-    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
+    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+
+          
+     -- Neotree plugin
+    {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", 
+      "MunifTanjim/nui.nvim",
+    }
+}
 
 
     -- add your plugins here
@@ -55,6 +70,9 @@ require("lazy").setup({
 local builtin =require("telescope.builtin")
 vim.keymap.set('n','<C-p>' , builtin.find_files,{})
 vim.keymap.set('n','<leader>fg',builtin.live_grep,{})
+
+--Neotree key map 
+vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>',{})
 
 local configs =require("nvim-treesitter.configs")
  configs.setup({
